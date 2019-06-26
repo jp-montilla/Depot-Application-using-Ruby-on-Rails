d2e672879587b6295064ca67454d18be164c8f25
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-06-26 13:13:19 +0800

    Cart

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root 'store#index', as: 'store_index'
 
   resources :products
+
+  resources :line_items
+
+  resources :carts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
