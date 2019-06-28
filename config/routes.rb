95b457c0a041a73a632866306bedf1e1745fe1b4
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-06-28 16:11:07 +0800

    Atom Feeds

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
 
   resources :carts
 
+
+  resources :products do
+    get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
