0a963f94ebb35aec37a8fb7b54d5af2883b68570
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-06-30 11:29:24 +0800

    Updated Seed Content

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,28 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.create!(
-	title: 'Product 1',
+	title: 'Title of Product 1',
 	description: %{<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet congue nulla. Integer mollis feugiat dui quis tempus. Curabitur interdum lacinia semper. Suspendisse quis dolor non dolor ultrices sollicitudin. Donec neque nibh, euismod at placerat et, sagittis vel elit. Nullam fermentum vitae justo et congue. Pellentesque auctor faucibus nisi, ut mollis libero volutpat laoreet.</p>},
 	image_url: 'product1.jpg',
 	price: 100.00
 )
 
 Product.create!(
-	title: 'Product 2',
+	title: 'Title of Product 2',
 	description: %{<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet congue nulla. Integer mollis feugiat dui quis tempus. Curabitur interdum lacinia semper. Suspendisse quis dolor non dolor ultrices sollicitudin. Donec neque nibh, euismod at placerat et, sagittis vel elit. Nullam fermentum vitae justo et congue. Pellentesque auctor faucibus nisi, ut mollis libero volutpat laoreet.</p>},
 	image_url: 'product2.jpg',
 	price: 200.00
 )
 
 Product.create!(
-	title: 'Product 3',
+	title: 'Title of Product 3',
 	description: %{<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet congue nulla. Integer mollis feugiat dui quis tempus. Curabitur interdum lacinia semper. Suspendisse quis dolor non dolor ultrices sollicitudin. Donec neque nibh, euismod at placerat et, sagittis vel elit. Nullam fermentum vitae justo et congue. Pellentesque auctor faucibus nisi, ut mollis libero volutpat laoreet.</p>},
 	image_url: 'product3.jpg',
 	price: 300.00
 )
 
-Product.create!(	title: 'Product 4',
+Product.create!(	
+	title: 'Title of Product 4',
 	description: %{<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet congue nulla. Integer mollis feugiat dui quis tempus. Curabitur interdum lacinia semper. Suspendisse quis dolor non dolor ultrices sollicitudin. Donec neque nibh, euismod at placerat et, sagittis vel elit. Nullam fermentum vitae justo et congue. Pellentesque auctor faucibus nisi, ut mollis libero volutpat laoreet.</p>},
 	image_url: 'product4.jpg',
 	price: 400.00
