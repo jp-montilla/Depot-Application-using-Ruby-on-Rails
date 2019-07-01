c80f7f1ea35f2b0fbc0048ee2c4f65588c16abeb
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-07-01 13:36:22 +0800

    Finished React Drop Down

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import NoPayType
-import CreditCardPayType import CheckPayType
-import PurchaseOrderPayType
-from './NoPayType';
-from './CreditCardPayType'; from './CheckPayType';
-from './PurchaseOrderPayType';
+import NoPayType from './NoPayType';
+import CreditCardPayType from './CreditCardPayType';
+import CheckPayType from './CheckPayType';
+import PurchaseOrderPayType from './PurchaseOrderPayType';
+ 
 
 class PayTypeSelector extends React.Component { 
 	constructor(props){
@@ -14,22 +13,37 @@ class PayTypeSelector extends React.Component {
 	}
 
 	onPayTypeSelected(event){
-		console.log(event.target.value)
+		// console.log(event.target.value);
 		this.setState({ selectedPayType: event.target.value });
 	}
 
 	render() {
-		return (
-			<div className="field">
-				<label htmlFor="order_paytype">Pay type</label> 
-				<select onChange={this.onPayTypeSelected} id="paytype" name="order[paytype]">
-					<option value="">Select a payment method</option> 
-					<option value="Check">Check</option>
-					<option value="Credit card">Credit card</option> 
-					<option value="Purchase order">Purchase order</option>
-			   </select>
-	    </div>
-		); 
+		let PayTypeCustomComponent = NoPayType;
+		if (this.state.selectedPayType == "Credit card") {
+    	PayTypeCustomComponent = CreditCardPayType;
+		}
+		else if (this.state.selectedPayType == "Check"){
+			PayTypeCustomComponent = CheckPayType;
+		}
+		else if (this.state.selectedPayType == "Purchase order") {
+			PayTypeCustomComponent = PurchaseOrderPayType;
+		}
+		
+		return(
+			<div>
+				<div className = "field">
+					<label htmlFor="order_paytype">Pay type</label>
+					<select onChange={this.onPayTypeSelected}  name="order[pay_type]" >
+						<option value="">Select a payment method</option>
+						<option value="Check">Check</option>
+						<option value="Credit card">Credit card</option>
+						<option value="Purchase order">Purchase order</option>
+					</select>
+				</div>
+				<PayTypeCustomComponent />
+			</div>
+		);
+
 	}
 }
 export default PayTypeSelector
