6fd6cc4d66a9e167bbc66627abce15ef37879ba0
John Paul Montilla <johnpaul.montilla1315@gmail.com>
2019-07-08 13:19:55 +0800

    edited js file

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -7,7 +7,7 @@ class StoreController < ApplicationController
     if params[:set_locale]
       redirect_to store_index_url(locale: params[:set_locale]) 
     else
-      @products = Product.order(:title).page(params[:page]).per(3)
+      @products = Product.order(created_at: :desc).page(params[:page]).per(3)
       respond_to do |format|
         format.js {render 'index.js.erb'}
         format.html
